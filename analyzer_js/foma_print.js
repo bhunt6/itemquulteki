151dc9471ecff6b12ca9da669fc3b4c452aabcd2
BHunt <benjaminhunt93@gmail.com>
2024-07-21 03:53:58 -0500

    add links to dictionary on parse terms

--- analyzer_js/foma_print.js
+++ analyzer_js/foma_print.js
@@ -11,8 +11,18 @@ function fomaPrint(tkn, valid) {
 	else {
 		tkn.sort((a, b) => a.length - b.length);
 		document.getElementById("parse").style.display = "flex";
-		for (var i = 0; i < tkn.length; i++) {
-			content.innerHTML += `<p>&#8227; ${tkn[i].join(" + ")}</p>`;
+		for (let i = 0; i < tkn.length; i++) {
+			content.innerHTML += `&#8227; `; 
+			for (let j = 0; j<tkn[i].length-1; j++){
+				console.log(tkn[i][j]);
+				if(j!=0) {
+					content.innerHTML += `<a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=-${tkn[i][j].toLowerCase().replace(/[\@\–\-\+\?\±\%\:\₁]/g, "").replace(/\<su[bp]\>[\d*ef]\<\/su[bp]\>/g, "").replace(/\[.*\]/g,"").replace(/\~[sf]*/g, "").replace(/\(.*\)/g,"")}-&lang=akuz">${tkn[i][j]}</a> + `;
+				}
+				else {
+					content.innerHTML += `<a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=${tkn[i][j].toLowerCase()}&lang=akuz">${tkn[i][j]}</a> + `;
+				}
+			}
+			content.innerHTML += `${tkn[i][tkn[i].length-1]}`;
 			content.innerHTML += "<br />".repeat(2);
 		}
 		results.appendChild(content);
