e91116de905985d38dd8e113205116773af9cf43
BHunt <benjaminhunt93@gmail.com>
2024-07-22 21:30:58 -0500

    refactor to catch enclitics

--- analyzer_js/foma_print.js
+++ analyzer_js/foma_print.js
@@ -9,7 +9,6 @@ function fomaPrint(tkn, valid) {
 		"[N→N]"
 	]; 
 	var re = new RegExp(ptn.join("|"), "i");
-	var final = [];
 
 	results.innerHTML = "";
 	if(!valid) {
@@ -20,20 +19,20 @@ function fomaPrint(tkn, valid) {
 		tkn.sort((a, b) => a.length - b.length);
 		document.getElementById("parse").style.display = "flex";
 		for (let i = 0; i < tkn.length; i++) {
+			let final = [];
 			content.innerHTML += `&#8227; `; 
-			for (let j = 0; j<tkn[i].length-1; j++){
-				console.log(tkn[i][j]);
-				if(j!=0) {
-					content.innerHTML += `<a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=-${tkn[i][j].toLowerCase().replace(/[\@\–\-\+\?\±\%\:\₁]/g, "").replace(/\<su[bp]\>[\d*ef]\<\/su[bp]\>/g, "").replace(/\[.*\]/g,"").replace(/\~[sf]*/g, "").replace(/\(.*\)/g,"")}-&lang=akuz">${tkn[i][j]}</a> `;
+			for (let j = 0; j<tkn[i].length; j++){
+				if(j==0) { 
+					content.innerHTML += `<a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=${tkn[i][j].toLowerCase()}&lang=akuz">${tkn[i][j]}</a> + `;
 				}
-				else if(!re.test(tkn[i][j])) {
-					final.append(tkn[i][j]);
+				else if(!tkn[i][j].includes("→")) {
+					final.push(tkn[i][j]);
 				}
-				else{ 
-					content.innerHTML += `+ <a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=${tkn[i][j].toLowerCase()}&lang=akuz">${tkn[i][j]}</a> `;
+				else {
+					content.innerHTML += `<a target="_blank" rel="noopener noreferrer" href="https://bhunt6.github.io/akuzipigestun-sangaawa/results.html?search=-${tkn[i][j].toLowerCase().replace(/[\@\–\-\+\?\±\%\:\₁]/g, "").replace(/\<su[bp]\>[\d*ef]\<\/su[bp]\>/g, "").replace(/\[.*\]/g,"").replace(/\~[sf]*/g, "").replace(/\(.*\)/g,"")}-&lang=akuz">${tkn[i][j]}</a> + `;
 				}
 			}
-			content.innerHTML += final.join("+ ");
+			content.innerHTML += final.join(" + ");
 			content.innerHTML += "<br />".repeat(2);
 		}
 		results.appendChild(content);
